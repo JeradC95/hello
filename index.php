f1fc6fc94d442d63004df226ff46fc4bc07c1beb
Jerad <Jclark74@mail.greenriver.edu>
2021-01-14 15:33:05 -0500

    Added home page

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+//This is my controller
+
 //err reporting
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -10,6 +12,12 @@ require_once('vendor/autoload.php');
 //instantiate base class
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
+
+// define a default route (home page)
+$f3->route("GET /", function(){
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
 
 //run fat free
 $f3->run();
